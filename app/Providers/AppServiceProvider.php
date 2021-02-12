54872f49a085c5f7e48da2ffc2d92d21d795a497
Saad Mehmood <saadmehmood758@gmail.com>
2021-02-12 18:38:01 +0500

    laravel image library

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
     }
 
